fd4a7a6a5be8bf72aa54be9c279851dfa52e2262
Nikolay Chaykovskiy <nickrer1988@gmail.com>
2023-08-27 23:09:04 +0500

    15-cloud-01-network

---
+++ cloud-provider/15-cloud-01-network/yc-tf/provider.tf
@@ -0,0 +1,14 @@
+# Configuring the provider
+
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.47.0"
+    }
+  }
+}
+
+provider "yandex" {
+  folder_id = var.folder_id
+}
